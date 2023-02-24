e92abe88cf2f17ae7a9534c6cf5169af45d37ade
Wenderson Oliveira <wnoliveira@ucs.br>
2023-02-23 23:01:42 -0300

    Adicionando post *Hello world em C com CodeBlocks*.

---
+++ Hello world em C com CodeBlocks/Hello world em C com CodeBlocks/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\r\n#############################################");
+    printf("\r\n# Hello world from: programacao eletronica! #");
+    printf("\r\n#############################################");
+    printf("\r\n# Lang: C                                   #");
+    printf("\r\n# IDE: Code::Blocks                         #");
+    printf("\r\n#############################################");
+
+    (void)getchar();
+
+    return 0;
+}
